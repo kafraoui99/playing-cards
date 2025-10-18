bd060cf615baed87d0d549b8c1f0d9e2234c6a0b
kafraoui99 <93958833+kafraoui99@users.noreply.github.com>
2025-10-18 18:42:12 +0200

    Suppression des fichiers html css du premier composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  standalone: true, // tjrs a True 
+  template:'<h1>Hello World</h1>'
 })
 export class AppComponent {
-  title = 'playing-cards';
 }
